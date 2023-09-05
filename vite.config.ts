d61102f94d0ef9442eecab54c79176f63b1d10b1
asiaznik <artur@vegaprotocol.io>
2023-09-05 11:19:57 +0200

    clean README

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["./tsconfig.json", "./tsconfig.node.json"],
-    tsconfigRootDir: __dirname,
-  },
   plugins: [react()],
 });
